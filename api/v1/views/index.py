b2f72f79bdd30f998305e83240e1839683c4e448
irafajs <irafajs@gmail.com>
2024-01-09 00:38:10 +0200

    CORRECTED

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -19,12 +19,15 @@ def status():
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
     """retrieve number of of each objects"""
-    stats = {
-            'amenities': storage.count('Amenity'),
-            'cities': storage.count('City'),
-            'places': storage.count('Place'),
-            'reviews': storage.count('Review'),
-            'states': storage.count('State'),
-            'users': storage.count('User')
-            }
-    return jsonify(stats)
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+    datas = [City, Review, User, State, Place, Amenity]
+    d_id = ["cities", "reviews", "users", "states", "places", "amenities"]
+    obj_arr = {}
+    for count in range(len(datas)):
+        obj_arr[d_id[count]] = storage.count(datas[count])
+    return jsonify(obj_arr)
