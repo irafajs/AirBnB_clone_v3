662538ca01d483b93e85bf2cabd8308b56ec2fe7
irafajs <irafajs@gmail.com>
2024-01-08 19:32:46 +0200

    TASK04 API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,10 +5,26 @@ Shebang to make a py script
 
 
 from api.v1.views import app_views
+import json
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """method to return a json response format"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """retrieve number of of each objects"""
+    stats = {
+            'amenities': storage.count('Amenity'),
+            'cities': storage.count('City'),
+            'places': storage.count('Place'),
+            'reviews': storage.count('Review'),
+            'states': storage.count('State'),
+            'users': storage.count('User')
+            }
+    return jsonify(json.loads(json.dumps(stats, indent=2)))
