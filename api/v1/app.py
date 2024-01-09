fecc5de9e65f7cd8e00b72777316e6a6e091664b
irafajs <irafajs@gmail.com>
2024-01-09 02:42:29 +0200

    LATEST UPDATE CORRECTION

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,11 +7,14 @@ from flask import Flask, jsonify
 from os import getenv
 from api.v1.views import app_views
 from models import storage
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 app.register_blueprint(app_views)
+
+CORS(app, resources={r"/api/v1/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
